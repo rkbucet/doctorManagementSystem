726b157bb985793aab39868d93e4597bda7e8d21
Rahul Kumar Bhagat <rkb.ucet@gmail.com>
2024-02-09 05:54:13 +0530

    project, App and models created

---
+++ doctorManagementSystem/doctorApp/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Speciality(models.Model):
+    specialist = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.specialist
+
+class Location(models.Model):
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.location
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=255)
+    speciality = models.ForeignKey(Speciality, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    fees = models.IntegerField()
+    experience = models.IntegerField()
+    
